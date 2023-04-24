d3a32f8a40bd8c86c298e7d6919a0f31be80039e
pansaleon <rici_chuy@hotmail.com>
2023-04-23 23:00:13 -0600

    Added comment in TutorialModClient.java

--- src/main/java/net/pansadev/tutorialmod/TutorialModClient.java
+++ src/main/java/net/pansadev/tutorialmod/TutorialModClient.java
@@ -2,6 +2,7 @@ package net.pansadev.tutorialmod;
 
 import net.fabricmc.api.ClientModInitializer;
 
+//Comment related to code
 public class TutorialModClient implements ClientModInitializer {
     @java.lang.Override
     public void onInitializeClient() {
